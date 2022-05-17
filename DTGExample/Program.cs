64de44333fdadc9960e05401d4207ba6b1d48e93
Sven Roos PC1 <sven.roos@outlook.de>
2022-05-17 14:24:06 +0200

    1st Version

---
+++ DTGExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DateTimeGroup;
+
+
+
+namespace DTGExample
+{
+    internal class DTGExample
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Example for DTG");
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            DateTime dt = new DateTime();
+            dt = DateTime.Now;
+            Console.WriteLine("Current time as DTG for Z: " + dt.ToDTGString());
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please input a time as DTG: ");
+                string? input = Console.ReadLine();
+
+                if (DTG.IsValidDTG(input))
+                {
+                    dt = DTG.FromString(input);
+                    Console.WriteLine("Input date and time as UTC " + dt.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid DTG");
+                }
+            }
+
+
+        }
+    }
+}
